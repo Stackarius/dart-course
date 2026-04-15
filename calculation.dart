6e90835526dd6636500dfe5c6d8930a7207d7a6c
Adepoju Opeyemi <opepoju@gmail.com>
2026-04-15 15:49:42 +0100

    feat: Basic addition program using dart

---
+++ calculation.dart
@@ -0,0 +1,30 @@
+void main() {
+  int number1 = 10;
+  int num2 = 20;
+
+  // Addition
+  print(number1 + num2);
+  int food = number1 + num2;
+  print(food);
+
+  // Subtraction
+  print("Subtraction:  ");
+  print(number1 - num2);
+  int ball = number1 - num2;
+  int IB = number1 - num2;
+  print('This is Ib: $IB');
+  print(ball);
+
+  // Multiplication
+  print("Multiplication:  ");
+  int Daniel = number1 * num2;
+  print(Daniel);
+
+  // DIVISiON
+  print("Division:  ");
+  int number3 = 7;
+  int number4 = 3;
+
+  var run = number3 / number4;
+  print(run);
+}
