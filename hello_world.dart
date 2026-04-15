9899c4f41d90e14979a3d96834344ff78885ad5e
Adepoju Opeyemi <opepoju@gmail.com>
2026-04-15 20:28:37 +0100

    feat: Work on  string interpolation

--- hello_world.dart
+++ hello_world.dart
@@ -1,5 +1,10 @@
 void main() {
-  print("Hello World");
+  var firstName = 'John';
+  var lastName = 'Doe';
+  print("My name is $firstName $lastName");
+
+  var name = 8;
+  print('My name is $name');
 
   // This is a single line comment
 }
