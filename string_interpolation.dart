3a0d4ea7c334e8b9ce7393632df39befcd201845
Adepoju Opeyemi <opepoju@gmail.com>
2026-04-16 16:07:28 +0100

    feat: practice string interpolation

---
+++ string_interpolation.dart
@@ -0,0 +1,13 @@
+void main() {
+  String schoolName = "Anettcom Technologies";
+  String address = "Grace Cooking gas";
+
+  print("$schoolName is at $address");
+
+  String multiText = '''
+    Anettcom Technologies Academy
+    is at Grace Cooking gas,
+    Bolanle, Osun State
+''';
+  print("$multiText");
+}
