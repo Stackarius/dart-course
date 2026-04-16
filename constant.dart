bccd56fcf7128e63dee8701f5c084e022183d94b
Adepoju Opeyemi <opepoju@gmail.com>
2026-04-16 16:06:43 +0100

    feat: work on constant variables

---
+++ constant.dart
@@ -0,0 +1,17 @@
+void main() {
+  const g = 3.00;
+  print(g);
+
+  int num2 = 10;
+  print(num2);
+
+  num2 = 4;
+  print(num2);
+  // print(pi);
+
+  var fullname = 0;
+  print(fullname);
+
+  var fullName = 10;
+  print(fullName);
+}
